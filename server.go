6e4e6759b5d7dacae33c19420b4c5c1b4b58f441
TomasSorgetti <tomassorgetti456@gmail.com>
2024-09-23 00:59:42 -0300

    chore: primeros steps del archivo main

--- server.go
+++ server.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct{
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Artist string `json:"artist"`
+}
+var albums = []album{
+	{ID: "1", Title: "Blue", Artist: "John"},
+	{ID: "2", Title: "Green", Artist: "Sarah"},
+	{ID: "3", Title: "Red", Artist: "Bob"},	
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
 }
